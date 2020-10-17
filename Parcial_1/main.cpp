aae6cd4ceb4909bfbbf7d477fd8460055c13976c
angeelorenaoc <angee.ocampo@udea.edu.co>
2020-10-17 17:00:26 -0500

    Desarrollo de la opcion 1

--- Parcial_1/main.cpp
+++ Parcial_1/main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int main()
 {
-    int xa=0,xe,ya,ye,anga,ange, opcion=1;
-    cout << "El programa definirá las coordenadas de impacto de las balas." << endl;
+    float xa=0,xe,ya,ye,anga,ange;
+    int  opcion=1;
+    cout << "El programa definira las coordenadas de impacto de las balas." << endl;
     cout << "Posicion en y del canion amigo: " << endl;
     cin >> ya;
     cout << "Posicion en x del canion enemigo: " << endl;
@@ -23,30 +24,36 @@ int main()
         cout << "3.Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el ";
         cout<<"canion defensivo sea destruido sin importar si el cañón ofensivo pueda ser destruido." << endl;
         cout << "4.Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los";
-        cout << "cañones defensivo y ofensivo puedan ser destruidos." << endl;
+        cout << "caniones defensivo y ofensivo puedan ser destruidos." << endl;
         cout << "Elija la opción o 0 para salir:" << endl;
         cin >> opcion;
+        switch (opcion) {
+            case 0:{break;}
+            case 1:{
+                xe = xe - (0.05*xe);
+                ye = ye - (0.05*ye);
+                float vi=0, t = 0,vf=0;
+                t = sqrt((ye-(xe*tan (anga)))/-4.9);
+                vi = xe / (cos(anga)*t);
+                vf = (vi*sin(anga))-(9.8*t);
+                cout << "La bala impactara el canion defensivo si se lanza con una velocidad inicial: "<< vi << endl;
+                cout << "Y el tiempo en el que explotara la bala es:" << t << " .Y su velocidad final sera: " << vf;
+            }break;
+            case 2:{
+
+            }break;
+            case 3:{
+
+            }break;
+            case 4:{
+
+            }break;
+            default:{
+                cout << "Opcion incorrecta." << endl;
+            }break;
+        }
     }
-    switch (opcion) {
-        case 0:{break;}
-        case 1:{
-            int vi=0, t = 0,vf=0;
 
-
-        }break;
-        case 2:{
-
-        }break;
-        case 3:{
-
-        }break;
-        case 4:{
-
-        }break;
-        default:{
-            cout << "Opcion incorrecta." << endl;
-        }break;
-    }
 
 
 

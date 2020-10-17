d9b569e3af8aa4882a136fe996fd955fc4fd62d3
angeelorenaoc <angee.ocampo@udea.edu.co>
2020-10-17 16:20:07 -0500

    Terminacion declaracion de variables

--- Parcial_1/main.cpp
+++ Parcial_1/main.cpp
@@ -4,15 +4,13 @@ using namespace std;
 
 int main()
 {
-    int xa,xe,ya,ye,anga,ange, opcion=1;
+    int xa=0,xe,ya,ye,anga,ange, opcion=1;
     cout << "El programa definirá las coordenadas de impacto de las balas." << endl;
-    cout << "Posición en x del canion amigo: " << endl;
-    cin >> xa;
-    cout << "Posición en y del canion amigo: " << endl;
+    cout << "Posicion en y del canion amigo: " << endl;
     cin >> ya;
-    cout << "Posición en x del canion enemigo: " << endl;
+    cout << "Posicion en x del canion enemigo: " << endl;
     cin >> xe;
-    cout << "Posición en y del canion amigo: " << endl;
+    cout << "Posicion en y del canion amigo: " << endl;
     cin >> ye;
     cout << "Angulo sobre la horizontal del canion amigo: " << endl;
     cin >> anga;
@@ -20,6 +18,31 @@ int main()
     cin >> ange;
     while (opcion != 0){
         cout << "1.Generar disparos ofensivos que comprometan la integridad del canion defensivo." << endl;
+        cout << "2.Generar disparos defensivos que comprometan la integridad del canion ofensivo." << endl;
+        cout << "3.Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el ";
+        cout<<"canion defensivo sea destruido sin importar si el cañón ofensivo pueda ser destruido." << endl;
+        cout << "4.Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los";
+        cout << "cañones defensivo y ofensivo puedan ser destruidos." << endl;
+        cout << "Elija la opción o 0 para salir:" << endl;
+        cin >> opcion;
+    }
+    switch (opcion) {
+        case 0:{break;}
+        case 1:{
+
+        }break;
+        case 2:{
+
+        }break;
+        case 3:{
+
+        }break;
+        case 4:{
+
+        }break;
+        default:{
+            cout << "Opcion incorrecta." << endl;
+        }break;
     }
 
 

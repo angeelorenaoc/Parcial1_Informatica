969968febb2284528ff3605980686885f26316b8
angeelorenaoc <angee.ocampo@udea.edu.co>
2020-10-17 16:01:57 -0500

    Inicio del programa

---
+++ Parcial_1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int xa,xe,ya,ye,anga,ange, opcion=1;
+    cout << "El programa definirá las coordenadas de impacto de las balas." << endl;
+    cout << "Posición en x del canion amigo: " << endl;
+    cin >> xa;
+    cout << "Posición en y del canion amigo: " << endl;
+    cin >> ya;
+    cout << "Posición en x del canion enemigo: " << endl;
+    cin >> xe;
+    cout << "Posición en y del canion amigo: " << endl;
+    cin >> ye;
+    cout << "Angulo sobre la horizontal del canion amigo: " << endl;
+    cin >> anga;
+    cout << "Angulo sobre la horizontal del canion amigo: " << endl;
+    cin >> ange;
+    while (opcion != 0){
+        cout << "1.Generar disparos ofensivos que comprometan la integridad del canion defensivo." << endl;
+    }
+
+
+
+}

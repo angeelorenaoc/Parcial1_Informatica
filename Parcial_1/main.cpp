5f0bbbc8358b67493d2db2075e00c8802dc867d5
angeelorenaoc <angee.ocampo@udea.edu.co>
2020-10-20 11:28:26 -0500

    Analisis del caso 3 y codificacion del caso 3 (Los valores dados por el caso 3 no han sido verificados)

--- Parcial_1/main.cpp
+++ Parcial_1/main.cpp
@@ -16,7 +16,7 @@ int main()
     cin >> ye;
     while (opcion != 0){
         cout << "1.Generar disparos (al menos 3) ofensivos que comprometan la integridad del canion defensivo." << endl;
-        cout << "2.Generar disparos defensivos que comprometan la integridad del canion ofensivo." << endl;
+        cout << "2.Generar disparos (al menos 3) defensivos que comprometan la integridad del canion ofensivo." << endl;
         cout << "3.Dado un disparo ofensivo, generar (al menos tres) disparos defensivos que impida que el ";
         cout<<"canion defensivo sea destruido sin importar si el canion ofensivo pueda ser destruido." << endl;
         cout << "4.Dado un disparo ofensivo, generar (al emnos tres) disparo defensivos que impidan que los";
@@ -54,7 +54,6 @@ int main()
                 }
                 float vi=0, t = 0,vf=0, angra = 0;
                 angra = (ange*pi)/180;
-                cout << angra << endl;
                 t = sqrt((-ye+ya+(xe*tan (angra)))/4.9);
                 vi = -xe / (cos(angra)*t);
                 vf = (vi*sin(angra))+(9.8*t);
@@ -65,6 +64,30 @@ int main()
             cout << "Recuerde que el signo de la velocidad solo indica la direccion." << endl << endl;
             }break;
             case 3:{
+            float VO0=0, yo=0, xo=0, angrao, angrad, t, vi,vf;
+            for (int i = 0; i<3; i++){
+                while (ange == 90 && anga == 90){
+                    cout << "El angulo que va a ingresar debe ser menor a 90 grados." << endl;
+                    cout << "Angulo sobre la horizontal del canion defensivo: " << endl;
+                    cin >> ange;
+                    cout << "Angulo sobre la horizontal del canion ofensivo: " << endl;
+                    cin >> anga;
+                    cout << "Velocidad incial con la que fue lanzada la bala del canion ofensivo: " << endl;
+                    cin >> VO0;
+                }
+                angrao = (anga*pi)/180;
+                angrad = (ange*pi)/180;
+                yo = ya +(VO0*sin(angrao)*(0.5)) - (4.9*0.5*0.5);
+                xo = xa + (VO0*cos(angrao)*0.5);
+                t = sqrt((ye-yo-(xe*tan (angrad)))/-4.9);
+                vi = xe / (cos(angrad)*t);
+                vf = (vi*sin(angrad))-(9.8*t);
+                cout << "La bala impactara el canion defensivo si se lanza con una velocidad inicial: "<< vi << endl;
+                cout << "Y el tiempo en el que explotara la bala es:" << t << " .Y su velocidad final sera: " << vf<< endl;
+                ange = 90;
+                anga = 90;
+            }
+
 
             }break;
             case 4:{
